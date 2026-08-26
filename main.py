00ad1487257fa4c13a2bd3b8a1bcf255dd0d555b
Hydrooxzgen <ATwbMail@163.com>
2026-08-26 22:44:31 +0800

    新增全新服务器bot假人功能 发布b0.3.0

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from uuid import uuid4
 ROOT = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PY = os.path.join(ROOT, "config.py")
 CONFIG_EXAMPLE = os.path.join(ROOT, "config.example.py")
-VERSION = "b0.3.0 dev19"
-DESCRIPTION = "bot shell测试通过、firstrun bot区域有bug等待修复" # 仅当不为None时从Github拉取更新日志，反之则直接显示该变量内容。
+VERSION = "b0.3.0"
+DESCRIPTION = None # 仅当不为None时从Github拉取更新日志，反之则直接显示该变量内容。
 GITHUB_REPO = "Hydrooxzgen/EnderBridge"  # You can edit this to your own repository if you fork it :)
 WANT_RESET = "--reset-all" in sys.argv
 WANT_EXPORT = "export" in sys.argv
