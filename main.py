392efc6c822d62d9b15cc48a6f7a4fd34c689fba
Hydrooxzgen <ATwbMail@163.com>
2026-08-29 14:37:16 +0800

    修复online无法加入服务的问题，realms待测试

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from uuid import uuid4
 ROOT = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PY = os.path.join(ROOT, "config.py")
 CONFIG_EXAMPLE = os.path.join(ROOT, "config.example.py")
-VERSION = "b0.3.3 dev4"
+VERSION = "b0.3.3 dev5"
 DESCRIPTION = "修复bug" # 仅当不为None时从Github拉取更新日志，反之则直接显示该变量内容。
 GITHUB_REPO = "Hydrooxzgen/EnderBridge"  # You can edit this to your own repository if you fork it :)
 WANT_RESET = "--reset-all" in sys.argv
